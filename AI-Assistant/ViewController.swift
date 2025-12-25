4c24d2a4b53c1dca47a82ac902c4686013b56389
jaber1212 <jjjjjgj5@gmail.com>
2025-12-25 12:29:59 -0800

    Initial Commit

---
+++ AI-Assistant/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AI-Assistant
+//
+//  Created by M.jaber on 25/12/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ AI-AssistantTests/AI_AssistantTests.swift
@@ -0,0 +1,17 @@
+//
+//  AI_AssistantTests.swift
+//  AI-AssistantTests
+//
+//  Created by M.jaber on 25/12/2025.
+//
+
+import Testing
+@testable import AI_Assistant
+
+struct AI_AssistantTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
